81aeaff365910c080cc1169409bee34164df1053
Federico Barbieri <federicobarbieri54@gmail.com>
2024-04-16 23:52:55 +0200

    only render end message after game is over

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import {VICTORY_MESSAGE} from "@/settings"
-defineProps<{
+import {DEFEAT_MESSAGE} from "@/settings"
+import { ref } from "vue";
 
-}>()
+defineProps<{ wordOfTheDay: String }>()
+
+const guessInProgress = ref("")
+const guessSubmitted = ref("")
+
 </script>
 
 <template>
-  <input type="text">
-  <p>{{ VICTORY_MESSAGE  }}</p>
-  <p>Better luck next time</p>
+  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress">
+  <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
+
 
 </template>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,15 +1,18 @@
 
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from '../../settings.ts'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings.ts'
 
 // Unit tests for the WordleBoard component
 
 describe('WordleBoard', () => {
+
+  let wordOfTheDay = "TESTS"
+
   test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
     
     // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
 
     // Act phase
     const guessInput = wrapper.find("input[type=text")
@@ -23,7 +26,7 @@ describe('WordleBoard', () => {
   test("a defeat message appears if the user makes a guess that is incorrect", async () => {
 
     // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
 
     // Act phase
     const guessInput = wrapper.find("input[type=text")
@@ -31,10 +34,20 @@ describe('WordleBoard', () => {
     await guessInput.trigger("keydown.enter")
 
     // Assert phase
-    expect(wrapper.text()).toContain("Better luck next time")
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
 
   })
 
 
-  test.todo("no end-of-game appears if the user has not yet made a guess")
+  test.todo("no end-of-game appears if the user has not yet made a guess", async () => {
+
+    // Arrange phase
+    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+
+
+    // Assert phase
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+
+  })
 })
