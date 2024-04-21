71bb60c8cb54508c98f4a502b05f9ac5aaa7bca3
Federico Barbieri <federicobarbieri54@gmail.com>
2024-04-21 21:12:45 +0200

    organize tests

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -7,7 +7,45 @@ import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings.ts'
 
 describe('WordleBoard', () => {
 
-  let wordOfTheDay = "TESTS"
+
+   let wordOfTheDay = "TESTS"
+
+  describe("End of the game messages", () => {
+
+    // test 1
+
+    test("a defeat message appears if the user makes a guess that is incorrect", async () => {
+
+      // Arrange phase
+      const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+  
+      // Act phase
+      const guessInput = wrapper.find("input[type=text")
+      await guessInput.setValue("WRONG")
+      await guessInput.trigger("keydown.enter")
+  
+      // Assert phase
+      expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  
+    })
+  
+    // test 2
+  
+    test.todo("no end-of-game appears if the user has not yet made a guess", async () => {
+  
+      // Arrange phase
+      const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+  
+  
+      // Assert phase
+      expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+      expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+  
+    })
+
+  })
+
+  describe("Rules for defining word of the day", () => {
 
   test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
     
@@ -23,33 +61,8 @@ describe('WordleBoard', () => {
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 
-  test("a defeat message appears if the user makes a guess that is incorrect", async () => {
+  // test 2
 
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-
-    // Act phase
-    const guessInput = wrapper.find("input[type=text")
-    await guessInput.setValue("WRONG")
-    await guessInput.trigger("keydown.enter")
-
-    // Assert phase
-    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
-
-  })
-
-
-  test.todo("no end-of-game appears if the user has not yet made a guess", async () => {
-
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-
-
-    // Assert phase
-    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
-    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
-
-  })
 
   test("If a word of the day provided does not have exactly 5 characters, a warning is emitted", async () => {
 
@@ -62,6 +75,8 @@ describe('WordleBoard', () => {
     
   })
 
+  // test 3
+
   test("If a word of the day is not all in uppercase, a warning is emitted", async() => {
 
     console.warn = vi.fn()
@@ -72,6 +87,8 @@ describe('WordleBoard', () => {
     expect(console.warn).toHaveBeenCalled()
 
   })
+
+  // test 4
 
   test("If a word of the day is not a real word, a warning is emitted", async() => {
 
@@ -84,6 +101,8 @@ describe('WordleBoard', () => {
 
   })
 
+  // test 5
+
   test("if a real word of 5 characters and uppercase is provided, no warning is emitted", async() => {
     console.warn = vi.fn()
 
@@ -93,12 +112,12 @@ describe('WordleBoard', () => {
     expect(console.warn).not.toHaveBeenCalled()
   })
 
-  test("if a word is numbers a warning is emitted", async() => {
-    console.warn = vi.fn()
-
-    // Arrange phase
-    mount(WordleBoard, {props: {wordOfTheDay: 123}})
-
-    expect(console.warn).toHaveBeenCalled()
   })
+
+  //describe("Player input", () => {
+
+ // })
+
+  
+
 })
