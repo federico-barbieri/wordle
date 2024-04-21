2cfc030c5cc383b9aa5717c38396bb28a7235b2c
Federico Barbieri <federicobarbieri54@gmail.com>
2024-04-21 21:19:29 +0200

    use data providers that inject words into the tests

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -31,7 +31,7 @@ describe('WordleBoard', () => {
   
     // test 2
   
-    test.todo("no end-of-game appears if the user has not yet made a guess", async () => {
+    test("no end-of-game appears if the user has not yet made a guess", async () => {
   
       // Arrange phase
       const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
@@ -43,39 +43,48 @@ describe('WordleBoard', () => {
   
     })
 
+    // test 3
+
+    test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
+    
+      // Arrange phase
+      const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+  
+      // Act phase
+      const guessInput = wrapper.find("input[type=text")
+      await guessInput.setValue("TESTS")
+      await guessInput.trigger("keydown.enter")
+  
+      // Assert phase
+      expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+    })
+
   })
 
   describe("Rules for defining word of the day", () => {
 
-  test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
-    
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-
-    // Act phase
-    const guessInput = wrapper.find("input[type=text")
-    await guessInput.setValue("TESTS")
-    await guessInput.trigger("keydown.enter")
-
-    // Assert phase
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
-  })
-
-  // test 2
+    test.each(
+      [
+        "FLY", "tests", "QWERT"
+      ]
+    )
 
 
-  test("If a word of the day provided does not have exactly 5 characters, a warning is emitted", async () => {
+  // test 1
+
+
+  test("If '%s' does not have exactly 5 characters, a warning is emitted", async (wordOfTheDay) => {
 
     console.warn = vi.fn()
 
     // Arrange phase
-    mount(WordleBoard, {props: {wordOfTheDay: "FLY"}})
+    mount(WordleBoard, {props: {wordOfTheDay: wordOfTheDay}})
 
     expect(console.warn).toHaveBeenCalled()
     
   })
 
-  // test 3
+  // test 2
 
   test("If a word of the day is not all in uppercase, a warning is emitted", async() => {
 
@@ -88,7 +97,7 @@ describe('WordleBoard', () => {
 
   })
 
-  // test 4
+  // test 3
 
   test("If a word of the day is not a real word, a warning is emitted", async() => {
 
@@ -101,7 +110,7 @@ describe('WordleBoard', () => {
 
   })
 
-  // test 5
+  // test 4
 
   test("if a real word of 5 characters and uppercase is provided, no warning is emitted", async() => {
     console.warn = vi.fn()
